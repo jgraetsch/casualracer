adce5b175dc93e2ccd70aa10514cc440bbc195ea
jgraetsch <graetsch@msn.com>
2016-03-23 13:27:03 +0100

    Mittagspause

---
+++ CasualRacer/Model/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasualRacer.Model
+{
+    internal class Game
+    {
+        public List<Player> Players { get; set; }
+
+        public Game()
+        {
+            Players = new List<Player>();
+        }
+    }
+}
---
+++ CasualRacer/Model/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CasualRacer.Model
+{
+    internal class Player
+    {
+        public Vector Position { get; set; }
+
+        public Player()
+        {
+
+        }
+
+        
+    }
+}
